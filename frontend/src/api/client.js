97203ad000c1285838b8cdb5555dd402aeb4945c
Imadrana <imadrana@gmail.com>
2025-12-04 12:54:03 -0700

    Restore API helper exports in client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -8,6 +8,7 @@ const client = axios.create({
   baseURL,
 });
 
+// Attach JWT if present
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem("authToken");
   if (token) {
@@ -15,5 +16,26 @@ client.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// ----- Data APIs used by Dashboard -----
+
+export function getNutritionalInsights(dietType) {
+  const params = {};
+  if (dietType) params.dietType = dietType;
+  return client.get("/getNutritionalInsights", { params }).then((r) => r.data);
+}
+
+export function getClusters(k = 3) {
+  return client
+    .get("/getClusters", { params: { k } })
+    .then((r) => r.data);
+}
+
+export function getRecipes(dietType, page = 1, pageSize = 10, keyword = "") {
+  const params = { page, pageSize };
+  if (dietType) params.dietType = dietType;
+  if (keyword) params.q = keyword;
+  return client.get("/getRecipes", { params }).then((r) => r.data);
+}
 
 export default client;
